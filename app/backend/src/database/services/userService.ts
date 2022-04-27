2aed75dfe9047bf2b0ad5cf91e8030a9ce7714ef
Flávio Bianchetti <fjb.developer@gmail.com>
2022-04-27 16:53:55 -0300

    feat: User service included

---
+++ app/backend/src/database/services/userService.ts
@@ -0,0 +1,25 @@
+import { IUser, IUserToken } from '../interfaces';
+import User from '../models/User';
+import { CryptPass, Token } from '../utils';
+
+export default class UserService {
+  public static getUserInfo =
+  async (email: string, password: string): Promise<IUserToken | undefined> => {
+    const user: IUser | null = await User.findOne({ where: { email } });
+
+    if (!user || !user.id || !CryptPass.verify(password, user?.password || '')) return undefined;
+
+    const userInfo: IUser = {
+      id: user.id,
+      username: user.username,
+      role: user.role,
+      email: user.email,
+    };
+
+    const result = {
+      user: userInfo,
+      token: await Token.generate(userInfo),
+    };
+    return result as IUserToken;
+  };
+}
