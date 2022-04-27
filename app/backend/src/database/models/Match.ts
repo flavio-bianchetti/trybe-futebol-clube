de2ce08c574cf36b8b22bc59ffff5ee37e7f0cf7
Flávio Bianchetti <fjb.developer@gmail.com>
2022-04-27 16:37:42 -0300

    refactor: Match model changed

--- app/backend/src/database/models/Match.ts
+++ app/backend/src/database/models/Match.ts
@@ -1,82 +1,58 @@
 import { DataTypes, Model } from 'sequelize';
 import db from '.';
-import { IMatch } from '../interfaces';
 import Team from './Team';
-// import OtherModel from './OtherModel';
 
-class Match extends Model<IMatch> {
-  // public <campo>!: <tipo>;
-  public id: number;
+class Match extends Model {
+  public id!: number;
 
-  public homeTeam: number;
+  public homeTeam!: number;
 
-  public homeTeamGoals: number;
+  public homeTeamGoals!: number;
 
-  public awayTeam: number;
+  public awayTeam!: number;
 
-  public awayTeamGoals: number;
+  public awayTeamGoals!: number;
 
-  public inProgress: number;
+  public inProgress!: number;
 }
 
 Match.init({
-  // ... Campos
   id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
     autoIncrement: true,
-    field: 'id',
   },
   homeTeam: {
     type: DataTypes.INTEGER,
     allowNull: false,
-    field: 'home_team',
   },
   homeTeamGoals: {
     type: DataTypes.INTEGER,
     allowNull: false,
-    field: 'home_team_goals',
   },
   awayTeam: {
     type: DataTypes.INTEGER,
     allowNull: false,
-    field: 'away_team',
   },
   awayTeamGoals: {
     type: DataTypes.INTEGER,
     allowNull: false,
-    field: 'away_team_goals',
   },
   inProgress: {
     type: DataTypes.INTEGER,
     allowNull: false,
-    field: 'in_progress',
   },
 }, {
-  // ... Outras configs
   underscored: true,
   sequelize: db,
-  // modelName: 'example',
-  modelName: 'match',
-  // tableName: 'matches',
+  modelName: 'matches',
   timestamps: false,
 });
 
-/**
- * `Workaround` para aplicar as associations em TS:
- * Associations 1:N devem ficar em uma das instâncias de modelo
- * */
+Match.belongsTo(Team, { foreignKey: 'homeTeam', as: 'homeTeamFkTeam' }); // as .... relacionamento
+Match.belongsTo(Team, { foreignKey: 'awayTeam', as: 'awayTeamFkTeam' });
 
-// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
-// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
-
-Match.belongsTo(Team, { foreignKey: 'homeTeam', as: 'homeTeamFkMatch' }); // as .... relacionamento
-Match.belongsTo(Team, { foreignKey: 'awayTeam', as: 'awayTeamFkMatch' });
-
-// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
-// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
-
-Team.hasMany(Match, { foreignKey: 'homeTeam', as: 'homeTeamFkTeam' });
-Team.hasMany(Match, { foreignKey: 'awayTeam', as: 'homeTeamFkTeam' });
+Team.hasMany(Match, { foreignKey: 'homeTeam', as: 'homeTeamFkMatch' });
+Team.hasMany(Match, { foreignKey: 'awayTeam', as: 'awayTeamFkMatch' });
 
 export default Match;
