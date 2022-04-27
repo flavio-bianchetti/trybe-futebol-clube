895062f430af090a2cb556845d187a7c8d046ae5
Flávio Bianchetti <fjb.developer@gmail.com>
2022-04-27 16:56:45 -0300

    feat: User controller included

---
+++ app/backend/src/database/controllers/userController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import UserService from '../services';
+
+export default class UserController {
+  public static create = async (req: Request, res: Response) => {
+    try {
+      const { email, password } = req.body;
+      const result = await UserService.getUserInfo(email, password);
+
+      if (!result) return res.status(401).json({ message: 'Incorrect email or password' });
+
+      return res.status(200).json(result);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: err });
+    }
+  };
+}
