4927d03fd5a60246214adf45d0218b3fa8a1f3c5
Flávio Bianchetti <fjb.developer@gmail.com>
2022-04-27 16:51:42 -0300

    refactor: Model files export removed

--- app/backend/src/database/models/index.ts
+++ app/backend/src/database/models/index.ts
@@ -1,13 +1,4 @@
 import { Sequelize } from 'sequelize';
 import * as config from '../config/database';
-import Match from './Match';
-import Team from './Team';
-import User from './User';
 
 export default new Sequelize(config);
-
-export {
-  Match,
-  Team,
-  User,
-}
