7ccbfc73f6d657345ec69dd42a8a07e52f57c0fe
Flávio Bianchetti <fjb.developer@gmail.com>
2022-04-27 17:00:24 -0300

    feat: app.ts - Routes included

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import routes from './database/api/routes';
 
 class App {
   public app: express.Express;
@@ -20,6 +21,7 @@ class App {
 
     this.app.use(accessControl);
     this.app.use(express.json());
+    this.app.use('/', routes);
   }
 
   // ...
