49c22fabb1a6933653be8cffc5dc77f46f19a2e8
Flávio Bianchetti <fjb.developer@gmail.com>
2022-04-29 04:44:36 -0300

    feat: Team service findAll method included'

---
+++ app/backend/src/database/services/teamService.ts
@@ -0,0 +1,10 @@
+import { ITeam } from "../interfaces"
+import Team from "../models/Team"
+
+export default class TeamService {
+  public static findAll = async (): Promise<ITeam[] | undefined> => {
+    const teams = await Team.findAll();
+    if (!teams) return undefined;
+    return teams;
+  };
+}
