889fa0eee40a9c0c083308f47f73b2ba620abe03
Flávio Bianchetti <fjb.developer@gmail.com>
2022-04-29 05:11:27 -0300

    feat: Team controller route included

--- app/backend/src/database/api/routes.ts
+++ app/backend/src/database/api/routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import * as cors from 'cors';
-import UserController from '../controllers';
+import { UserController, TeamController } from '../controllers';
 import LoginValidatorMiddleware from '../middlewares';
 
 const routes = Router();
@@ -9,5 +9,7 @@ routes.use(cors());
 
 routes.post('/login', LoginValidatorMiddleware.validate, UserController.create);
 routes.get('/login/validate', UserController.getRole);
+
+routes.get('/teams', TeamController.findAll);
 
 export default routes;
