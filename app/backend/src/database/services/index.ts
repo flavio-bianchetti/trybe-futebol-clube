b4e485bec91225a0df44f82f1803f642a991fadd
Flávio Bianchetti <fjb.developer@gmail.com>
2022-04-29 13:38:47 -0300

    feat: Match service export included in index file

--- app/backend/src/database/services/index.ts
+++ app/backend/src/database/services/index.ts
@@ -1,7 +1,9 @@
 import UserService from './userService';
 import TeamService from './teamService';
+import MatchService from './matchService';
 
 export {
   UserService,
   TeamService,
+  MatchService,
 };
