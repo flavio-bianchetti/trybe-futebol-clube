b2993aef8bab4625df86856a9ff30abda67890a6
Flávio Bianchetti <fjb.developer@gmail.com>
2022-04-29 05:58:23 -0300

    feat: Find Team parameter validator included

---
+++ app/backend/src/database/middlewares/findTeamParameterValidator.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express';
+
+export default class FindTeamParameterValidator {
+  public static validate =
+  (req: Request, res: Response, next: NextFunction): Response | undefined => {
+    try {
+      const { id } = req.params;
+      if (!id || typeof Number(id) !== 'number') {
+        return res.status(421).json({ message: 'Invalid parameter' });
+      }
+      next();
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: err });
+    }
+  };
+}
