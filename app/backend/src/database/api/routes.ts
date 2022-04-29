d3a94c33b4d18ea5693fa47ff20818b980bc412f
Flávio Bianchetti <fjb.developer@gmail.com>
2022-04-29 13:56:23 -0300

    feat: Match findAll method included in routes

--- app/backend/src/database/api/routes.ts
+++ app/backend/src/database/api/routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import * as cors from 'cors';
-import { UserController, TeamController } from '../controllers';
+import { UserController, TeamController, MatchController } from '../controllers';
 import { LoginValidatorMiddleware, FindTeamParameterValidator } from '../middlewares';
 
 const routes = Router();
@@ -12,5 +12,7 @@ routes.get('/login/validate', UserController.getRole);
 
 routes.get('/teams', TeamController.findAll);
 routes.get('/teams/:id', FindTeamParameterValidator.validate, TeamController.findByPk);
+
+routes.get('/matches', MatchController.findAll);
 
 export default routes;
