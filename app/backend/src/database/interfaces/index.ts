6dd728b2596296fb549148eed159ad9c359ac90a
Flávio Bianchetti <fjb.developer@gmail.com>
2022-04-29 13:31:38 -0300

    feat: IMatchTeamsNames interface export included

--- app/backend/src/database/interfaces/index.ts
+++ app/backend/src/database/interfaces/index.ts
@@ -2,10 +2,12 @@ import IMatch from './IMatch';
 import ITeam from './ITeam';
 import IUser from './IUser';
 import IUserToken from './IUserToken';
+import IMatchTeamsNames from './IMatchTeamsNames';
 
 export {
   IMatch,
   ITeam,
   IUser,
   IUserToken,
+  IMatchTeamsNames,
 };
