ee4ade26bb330145a5f3d7a45a2140aefd819315
Flávio Bianchetti <fjb.developer@gmail.com>
2022-04-29 13:52:56 -0300

    feat: Match controller export included in index

--- app/backend/src/database/controllers/index.ts
+++ app/backend/src/database/controllers/index.ts
@@ -1,7 +1,9 @@
 import UserController from './userController';
 import TeamController from './teamController';
+import MatchController from './matchController';
 
 export {
   UserController,
   TeamController,
+  MatchController,
 };
