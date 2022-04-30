895477867f4d793e7707a57f7f050ec318b6ce28
Flávio Bianchetti <fjb.developer@gmail.com>
2022-04-30 08:12:24 -0300

    feat: Token validator included in routes

--- app/backend/src/database/api/routes.ts
+++ app/backend/src/database/api/routes.ts
@@ -1,18 +1,27 @@
 import { Router } from 'express';
 import * as cors from 'cors';
 import { UserController, TeamController, MatchController } from '../controllers';
-import { LoginValidatorMiddleware, FindTeamParameterValidator } from '../middlewares';
+import {
+  LoginValidatorMiddleware,
+  FindTeamParameterValidator,
+  TokenValidator,
+} from '../middlewares';
 
 const routes = Router();
 
 routes.use(cors());
 
 routes.post('/login', LoginValidatorMiddleware.validate, UserController.create);
-routes.get('/login/validate', UserController.getRole);
+routes.get('/login/validate', TokenValidator.validate, UserController.getRole);
 
-routes.get('/teams', TeamController.findAll);
-routes.get('/teams/:id', FindTeamParameterValidator.validate, TeamController.findByPk);
+routes.get('/teams', TokenValidator.validate, TeamController.findAll);
+routes.get(
+  '/teams/:id',
+  TokenValidator.validate,
+  FindTeamParameterValidator.validate,
+  TeamController.findByPk,
+);
 
-routes.get('/matches', MatchController.findAll);
+routes.get('/matches', TokenValidator.validate, MatchController.findAll);
 
 export default routes;
