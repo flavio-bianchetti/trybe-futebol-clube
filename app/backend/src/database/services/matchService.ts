fd3f02641fe7028d3c5e38c406ca872ea09809a6
Flávio Bianchetti <fjb.developer@gmail.com>
2022-05-02 18:58:19 -0300

    refactor: Match service findAll method changed

--- app/backend/src/database/services/matchService.ts
+++ app/backend/src/database/services/matchService.ts
@@ -1,26 +1,16 @@
-import { IMatchTeamsNames } from '../interfaces';
+import { IMatch } from '../interfaces';
 import Match from '../models/Match';
 import Team from '../models/Team';
 
 export default class MatchService {
-  public static findAll = async (): Promise<IMatchTeamsNames[]> => {
-    const matches = await Match.findAll({ raw: true });
-    const teams = await Team.findAll();
-    const result = [];
-
-    if (!matches) return [];
-
-    for (let index = 0; index < matches.length; index += 1) {
-      const teamHome = teams.find((team) => team.id === matches[index].homeTeam);
-      const teamAway = teams.find((team) => team.id === matches[index].awayTeam);
-      if (teamHome && teamAway) {
-        result.push({
-          ...matches[index],
-          teamHome: { teamName: teamHome.teamName },
-          teamAway: { teamName: teamAway.teamName },
-        } as IMatchTeamsNames);
-      }
-    }
-    return result;
+  public static findAll = async (): Promise<IMatch[]> => { // resolver este problema: Retorna IMatchTeamsNames[]
+    const matches = await Match.findAll({
+      include: [
+        { model: Team, as: 'teamHome', attributes: { exclude: ['id'] } },
+        { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } },
+      ],
+      raw: true,
+    });
+    return matches;
   };
 }
