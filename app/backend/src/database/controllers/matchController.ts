26bfe3c086d8cfd86020f2ec967f40627d3053d2
Flávio Bianchetti <fjb.developer@gmail.com>
2022-05-02 22:35:46 -0300

    feat: Match controller create method included

--- app/backend/src/database/controllers/matchController.ts
+++ app/backend/src/database/controllers/matchController.ts
@@ -12,4 +12,26 @@ export default class MatchController {
       return res.status(500).json({ error: err });
     }
   };
+
+  public static create =
+  async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const { homeTeam, awayTeam, homeTeamGoals, awayTeamGoals, inProgress } = req.body;
+      const result = await MatchService.create({
+        homeTeam, homeTeamGoals, awayTeam, awayTeamGoals, inProgress,
+      });
+      if (result === undefined) {
+        return res.status(401).json(
+          { message: 'It is not possible to create a match with two equal teams' },
+        );
+      }
+      if (result === null) {
+        return res.status(404).json({ message: 'There is no team with such id!' });
+      }
+      return res.status(201).json(result);
+    } catch (err) {
+      console.error(`Erro inicial ${err}`);
+      return res.status(500).json({ error: err });
+    }
+  };
 }
