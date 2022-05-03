41972b29a1051a24f229f02bccb1dc1a6c4a860c
Flávio Bianchetti <fjb.developer@gmail.com>
2022-05-02 22:29:25 -0300

    fix: comment insert to reminder for correction

--- app/backend/src/database/services/matchService.ts
+++ app/backend/src/database/services/matchService.ts
@@ -2,8 +2,10 @@ import { IMatch } from '../interfaces';
 import Match from '../models/Match';
 import Team from '../models/Team';
 
+// resolver este problema: Retorna IMatchTeamsNames[]
+// resolver problema do boleano
 export default class MatchService {
-  public static findAll = async (): Promise<IMatch[]> => { // resolver este problema: Retorna IMatchTeamsNames[]
+  public static findAll = async (): Promise<IMatch[]> => {
     const matches = await Match.findAll({
       include: [
         { model: Team, as: 'teamHome', attributes: { exclude: ['id'] } },
