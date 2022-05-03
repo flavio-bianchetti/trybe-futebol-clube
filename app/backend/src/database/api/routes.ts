8ef1383d3c779e89b1c4866248e0c47ee387eb18
Flávio Bianchetti <fjb.developer@gmail.com>
2022-05-02 22:39:59 -0300

    feat: Match controller & validator included routes

--- app/backend/src/database/api/routes.ts
+++ app/backend/src/database/api/routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 import * as cors from 'cors';
-import { UserController, TeamController, MatchController } from '../controllers';
+import {
+  UserController,
+  TeamController,
+  MatchController,
+} from '../controllers';
 import {
   LoginValidatorMiddleware,
   FindTeamParameterValidator,
   TokenValidator,
+  MatchValidatorMiddleware,
 } from '../middlewares';
 
 const routes = Router();
@@ -17,11 +22,16 @@ routes.get('/login/validate', TokenValidator.validate, UserController.getRole);
 routes.get('/teams', TeamController.findAll);
 routes.get(
   '/teams/:id',
-  TokenValidator.validate,
   FindTeamParameterValidator.validate,
   TeamController.findByPk,
 );
 
 routes.get('/matches', MatchController.findAll);
+routes.post(
+  '/matches',
+  TokenValidator.validate,
+  MatchValidatorMiddleware.validate,
+  MatchController.create,
+);
 
 export default routes;
