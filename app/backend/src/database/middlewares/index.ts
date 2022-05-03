cf4256a929476ecaa1e6687d5470bcd7711bb52f
Flávio Bianchetti <fjb.developer@gmail.com>
2022-05-02 22:35:02 -0300

    feat: Match Validator Middleware export included

--- app/backend/src/database/middlewares/index.ts
+++ app/backend/src/database/middlewares/index.ts
@@ -1,9 +1,11 @@
 import LoginValidatorMiddleware from './loginValidatorMiddleware';
 import FindTeamParameterValidator from './findTeamParameterValidator';
 import TokenValidator from './tokenValidator';
+import MatchValidatorMiddleware from './matchValidatorMiddleware';
 
 export {
   LoginValidatorMiddleware,
   FindTeamParameterValidator,
   TokenValidator,
+  MatchValidatorMiddleware,
 };
