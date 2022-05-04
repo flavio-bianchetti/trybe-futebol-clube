e33fedb35a5f353c931b93019f320f26b2f701b9
Flávio Bianchetti <fjb.developer@gmail.com>
2022-05-04 16:04:21 -0300

    feat: Update match validator export  in index

--- app/backend/src/database/middlewares/index.ts
+++ app/backend/src/database/middlewares/index.ts
@@ -2,10 +2,12 @@ import LoginValidatorMiddleware from './loginValidatorMiddleware';
 import FindTeamParameterValidator from './findTeamParameterValidator';
 import TokenValidator from './tokenValidator';
 import MatchValidatorMiddleware from './matchValidatorMiddleware';
+import UpdateMatchValidatorMiddleware from './updateMatchValidatorMiddleware';
 
 export {
   LoginValidatorMiddleware,
   FindTeamParameterValidator,
   TokenValidator,
   MatchValidatorMiddleware,
+  UpdateMatchValidatorMiddleware,
 };
