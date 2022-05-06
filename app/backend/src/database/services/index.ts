565796ed54ee1fb4ef953dc0cde93882c8f5b124
Flávio Bianchetti <fjb.developer@gmail.com>
2022-05-05 21:22:19 -0300

    feat: Leaderboard service export in index file

--- app/backend/src/database/services/index.ts
+++ app/backend/src/database/services/index.ts
@@ -1,9 +1,11 @@
 import UserService from './userService';
 import TeamService from './teamService';
 import MatchService from './matchService';
+import LeaderboardService from './leaderboardService';
 
 export {
   UserService,
   TeamService,
   MatchService,
+  LeaderboardService,
 };
