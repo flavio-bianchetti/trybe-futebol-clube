690a2aa22cf576c71d90847f3b1581f02498483c
Flávio Bianchetti <fjb.developer@gmail.com>
2022-05-05 21:23:32 -0300

    feat: Leaderboard controller export in index file

--- app/backend/src/database/controllers/index.ts
+++ app/backend/src/database/controllers/index.ts
@@ -1,9 +1,11 @@
 import UserController from './userController';
 import TeamController from './teamController';
 import MatchController from './matchController';
+import LeaderboardController from './leaderboardController';
 
 export {
   UserController,
   TeamController,
   MatchController,
+  LeaderboardController,
 };
